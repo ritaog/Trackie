d39282a91c69c5a74f7e97cc22df35802d7ffa43
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2021-11-21 16:06:41 -0700

    Includes functionality for Exercise routes

--- trackie/backend/routes/exercises.js
+++ trackie/backend/routes/exercises.js
@@ -3,6 +3,7 @@ import Exercise from "../models/exercise.model.js";
 
 const router = express.Router();
 
+//GET
 //localhost:5000/exercises/
 router.route("/").get(async (req, res) => {
   try {
@@ -14,6 +15,7 @@ router.route("/").get(async (req, res) => {
   }
 });
 
+//POST
 //localhost:5000/exercises/add
 router.route("/add").post(async (req, res) => {
   try {
@@ -28,12 +30,48 @@ router.route("/add").post(async (req, res) => {
       duration,
       date,
     });
-    /*
-    newExercise.save().then(() => res.json("Exercise added!"));
-    */
 
     const response = await newExercise.save();
     res.json("Exercise added!");
+  } catch (err) {
+    res.status(400).json("Error: " + err);
+  }
+});
+
+//GET
+//localhost:5000/exercises/:id
+router.route("/:id").get(async (req, res) => {
+  try {
+    const exercise = await Exercise.findById(req.params.id);
+    res.json(exercise);
+  } catch (err) {
+    res.status(400).json("Error: " + err);
+  }
+});
+
+//DELETE
+//localhost:5000/exercises/:id
+router.route("/:id").delete(async (req, res) => {
+  try {
+    const exercise = await Exercise.findByIdAndDelete(req.params.id);
+    res.json("Exercise deleted");
+  } catch (err) {
+    res.status(400).json("Error: " + err);
+  }
+});
+
+//POST
+//localhost:5000/exercises/update/:id
+router.route("/update/:id").post(async (req, res) => {
+  try {
+    const exercise = await Exercise.findById(req.params.id);
+    exercise.username = req.body.username;
+    exercise.description = req.body.description;
+    exercise.duration = Number(req.body.duration);
+    exercise.date = Date.parse(req.body.date);
+
+    const response = await exercise.save();
+    res.json("Exercise updated!");
   } catch (err) {
     res.status(400).json("Error: " + err);
   }
