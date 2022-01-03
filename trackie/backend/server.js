2ae6997260c8f6395548c01ce4533670a02d3322
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2022-01-03 16:58:49 -0700

    Moves database connection to a separate file

--- trackie/backend/server.js
+++ trackie/backend/server.js
@@ -4,8 +4,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import exercisesRouter from "./routes/exercises.js";
 import usersRouter from "./routes/users.js";
+import connectDB from "./config/db.js";
 
-dotenv.config();
+//dotenv.config();
+//Load config
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,12 +17,18 @@ app.use(cors());
 app.use(express.json());
 
 //connect to database
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+connectDB();
+/*
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
+*/
 
 //Routes
 //localhost:5000/exercises
