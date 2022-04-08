271e164c0f197ea00d3ceb56e15d4106590fc681
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2022-04-07 22:01:08 -0600

    Cleaned up server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import exercisesRouter from "./routes/exercises.js";
 import usersRouter from "./routes/users.js";
